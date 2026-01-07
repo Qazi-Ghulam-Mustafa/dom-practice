1adacf43206d306e77e5f34af2788a3aaa3e88b7
Qazi-Ghulam-Mustafa <Qazi-Ghulam-Mustafa>
2026-01-07 06:07:24 +0500

    Change Made

--- script.js
+++ script.js
@@ -1,91 +1,69 @@
+// Get Input Id's
+var userName = document.getElementById("userName");
+var contactNumber = document.getElementById("contactNumber");
+var age = document.getElementById("age");
 var email = document.getElementById("email");
 var password = document.getElementById("password");
-var rollNumber = document.getElementById("rollNumber");
+var cnic = document.getElementById("cnic");
+var CnicSuccessfuly = document.getElementById("CnicSuccessfuly")
+// Get Input Error Id's
+var nameError = document.getElementById("nameError");
+var contactNumberError = document.getElementById("contactNumberError");
+var ageError = document.getElementById("ageError");
 var emailError = document.getElementById("emailError");
 var passwordError = document.getElementById("passwordError");
 
+// html Tag => inner html
+//input value => value
 
-function checkStoragePassword(password) {
-    var capitalCheck = false;
-    var smallCheck = false;
-    var numberCheck = false;
-    var specialCheck = false;
 
-    for (var i = 0; i < password.length; i++) {
-        if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) {
-            capitalCheck = true;
-        } else if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) {
-            smallCheck = true;
-        } else if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57) {
-            numberCheck = true;
-        } else {
-            specialCheck = true;
-        }
+function submitBtn() {
+    if (userName.value == "") {
+        nameError.innerText = "Must Enter Your Name"
     }
-    var message = "missing:";
-    if (capitalCheck == false) {
-        message += "Capital Letter"
-    }
-    if (smallCheck == false) {
-        message += "Small Letter"
-    }
-    if (numberCheck == false) {
-        message += "Number"
-    }
-    if (specialCheck == false) {
-        message += "Special Character"
-    }
-    if (capitalCheck == true &&
-        smallCheck == true &&
-        numberCheck == true &&
-        specialCheck == true
-    ) {
-        return "True";
+    else if (userName.value.length < 5) {
+        nameError.innerText = "Name must Greater than 5 chracter"
     }
     else {
-        return message;
+        nameError.innerText = ""
     }
-}
 
-var arrayRollNum = [];
-
-function submit() {
     if (email.value == "") {
-       emailError.innerText = "Enter Your Email Adress";
-        emailError.style.color = "red";
-    } else if (!email.value.includes("@")) {
-        emailError.innerText = "Please Enter Valid Email";
-        emailError.style.color = "red";
-    } else if (password.value == "") {
-        passwordError.innerText = "Enter Your Email Password";
-        passwordError.style.color = "red";
-    } else if (password.value.length < 8) {
-        passwordError.innerText = "Password must be greater than 8 characters";
-        passwordError.style.color = "red";
-    } else {
-        var checkPassword = checkStoragePassword(password.value);
-        if (checkPassword == "True") {
-            document.getElementById("message").innerText = "Form Submitted Successfully";
-            document.getElementById("message").style.color = "green";
-        } else {
-            alert(checkPassword);
-        }
-        console.log(email.value)
-        console.log(password.value)
-        rollNumber.value = generateRollNumberFunction();
+        emailError.innerText = "Must Enter Your Email Address"
     }
-}
-function generateRollNumberFunction() {
-    var create = true;
-    while (create == true) {
-        var generateRollNumber = (Math.random() * 99999).toFixed(0);
-        if (!arrayRollNum.includes(generateRollNumber)) {
-            arrayRollNum.push(generateRollNumber);
-            create = false;
-            return generateRollNumber;
-        }
-        else {
-            create = true;
-        }
+    else if (!email.value.includes("@")) {
+        emailError.innerText = "Please Enter Valid Email Address"
+    }
+    else {
+        emailError.innerText = ""
+    }
+    if (password.value == "") {
+        passwordError.innerText = "Must Enter Your Email Password"
+    }
+    else if (password.value.length < 8) {
+        passwordError.innerText = "Email Passowrd Must Greater Than 8 Character"
+    }
+    else {
+        passwordError.innerText = ""
+    }
+
+    if (contactNumber.value == "" || contactNumber.value == 0) {
+        contactNumberError.innerText = "Must Enter Your Contact Number"
+    }
+    else if (contactNumber.value.length < 11) {
+        contactNumberError.innerText = "Your Contact Number must greater than 11 number"
+    }
+    else {
+        contactNumberError.innerText = ""
+    }
+
+    if (age.value == "" || age.value == 0) {
+        ageError.innerText = "Must Enter Your Age"
+    }
+    else if (age.value < 18) {
+        ageError.innerText = "Your are Eligible for CNIC"
+    }
+    else {
+      CnicSuccessfuly.innerText= "Your Cnic generate Successfully"
     }
 }
